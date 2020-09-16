084961d5caf5d16eaa7e9d62c3f3e0801f6aca39
phoz0n <33037084+phoz0n@users.noreply.github.com>
2020-09-16 22:30:42 +0200

    Added CubeStack
    Cube stacking on the car

--- Assets/GetBonus.cs
+++ Assets/GetBonus.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class GetBonus : MonoBehaviour
 {
+    public Transform CubeStack;
+    private int nbCube = 0;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.name);
-        Destroy(other.gameObject);
+        other.gameObject.transform.parent = gameObject.transform;
+        other.gameObject.transform.localPosition = CubeStack.localPosition;
+
+        float Z = other.gameObject.transform.localScale.z;
+        other.gameObject.transform.Translate(0, Z / 2 + nbCube++ * Z, 0, Space.Self);
     }
 }
