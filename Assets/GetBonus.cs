7600e3611f6f2839e51a9afc9f33a22a70d244d4
phoz0n <33037084+phoz0n@users.noreply.github.com>
2020-09-16 22:03:14 +0200

    Added bonus
    Added cubes on the road destroyed with trigger

---
+++ Assets/GetBonus.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetBonus : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        Destroy(other.gameObject);
+    }
+}
--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -9,15 +9,18 @@ public class MapGenerator : MonoBehaviour
     public GameObject roadSegment;
     public int SegmentQuantity = 5;
     public float Speed = 5f;
+    public GameObject bonus;
     private List<GameObject> InstantiatedRoads;
+    public int chance = 50;
     // Start is called before the first frame update
     void Start()
     {
+        Random.seed = (int)System.DateTime.Now.Ticks;
         InstantiatedRoads = new List<GameObject>();
 
         for (int i = 0; i < SegmentQuantity; i++)
         {
-            InstantiatedRoads.Add(Instantiate(roadSegment, new Vector3(0, 0, i * 10), Quaternion.identity));
+            SpawnRoad(i * 10);
         }
     }
 
@@ -45,8 +48,22 @@ public class MapGenerator : MonoBehaviour
             GameObject lastRoad = InstantiatedRoads[InstantiatedRoads.Count - 1];
             float LastZroadPosition = lastRoad.transform.position.z;
 
-            InstantiatedRoads.Add(Instantiate(roadSegment, new Vector3(0, 0, LastZroadPosition + 10), Quaternion.identity));
+            SpawnRoad(LastZroadPosition + 10);
         }
 
+    }
+    void SpawnRoad(float zPosition)
+    {
+        GameObject go = Instantiate(roadSegment, new Vector3(0, 0, zPosition), Quaternion.identity);
+        InstantiatedRoads.Add(go);
+        int random = Random.Range(0, 100);
+        if (random <= chance)
+        {
+            int randomGrid = Random.Range(0, 5);
+            float gridPosition = 0.8f * randomGrid - 1.6f;
+
+            GameObject bo = Instantiate(bonus, go.transform, false);
+            bo.transform.localPosition = new Vector3(gridPosition, bonus.transform.localScale.y / 2, 0);
+        }
     }
 }
